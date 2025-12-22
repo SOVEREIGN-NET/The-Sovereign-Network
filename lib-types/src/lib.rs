9c1ae414b7f2d415fd37b428711c43b89da5e8a8
petergrutherford <petergrutherford88@gmail.com>
2025-12-22 10:03:30 -0800

    fix test error

--- lib-types/src/lib.rs
+++ lib-types/src/lib.rs
@@ -2,11 +2,13 @@
 //! Stable, protocol-neutral, behavior-free.
 
 pub mod node_id;
+pub mod node_type;
 pub mod dht;
 pub mod chunk;
 pub mod errors;
 
 pub use node_id::NodeId;
+pub use node_type::NodeType;
 pub use dht::*;
 pub use chunk::*;
 pub use errors::*;
