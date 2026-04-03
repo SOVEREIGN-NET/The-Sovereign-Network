fa38c123bbaa181303dc57d1ea88b9bcad8953f7
umwelt <hugo@kode.zone>
2026-04-03 16:08:22 +0100

    chore: remove .gitnexus, stale docs, Windows scripts, stray root files (#2041) (#2042)

--- test_key_size.rs
+++
@@ -1,6 +0,0 @@
-﻿use lib_crypto::KeyPair;
-fn main() {
-    let keypair = KeyPair::generate().unwrap();
-    println!("dilithium_sk len: {}", keypair.private_key.dilithium_sk.len());
-    println!("kyber_sk len: {}", keypair.private_key.kyber_sk.len());
-}
